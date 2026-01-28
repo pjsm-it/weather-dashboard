d2171afa936f7dcbd98386ef7b17875755a1baf8
Paulo Melo <pjsm.it@proton.me>
2026-01-28 21:48:21 +0000

    feat(forecast): prepare forecast component state for future API integration

--- weather-dashboard-app/src/app/components/forecast/forecast.ts
+++ weather-dashboard-app/src/app/components/forecast/forecast.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-forecast',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './forecast.html',
   styleUrl: './forecast.css',
 })
 export class Forecast {
+  protected forecastDays: ForecastDay[] = [];
+}
 
+export interface ForecastDay {
+  date: string;
+  temperature?: number;
+  condition?: string;
 }
